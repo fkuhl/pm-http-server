1b864f98fc78a75fdc6c9609be3170dcf1f148a2
Frederick Kuhl <fkuhl@tyndalesoft.com>
2020-01-07 10:05:43 -0500

    Put PMDataTypes in Package.swift

--- Package.swift
+++ Package.swift
@@ -10,14 +10,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/http.git", from: "3.0.0"),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "0.1.3"),
+        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "0.2.0"),
+        .package(url: "file:///Users/fkuhl/Documents/workspace/pm-http/pm-data-types/PMDataTypes", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "pm-http-server",
-            dependencies: ["HTTP", "MongoSwift"]),
+            dependencies: ["HTTP", "MongoSwift", "PMDataTypes"]),
         .testTarget(
             name: "pm-http-serverTests",
             dependencies: ["pm-http-server"]),
