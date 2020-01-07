c62a23accff76cbe3dc0b3d4d995658754c9feb5
Frederick Kuhl <fkuhl@tyndalesoft.com>
2020-01-07 14:48:55 -0500

    Effort at better error reporting

--- Sources/pm-http-server/AsynchronousWork.swift
+++ Sources/pm-http-server/AsynchronousWork.swift
@@ -50,6 +50,7 @@ func processReadAll<D: DataType>(path: String,
         let matchingDocuments: [D] = try mongoProxy.readAll()
         return makeResponse(status: .ok, response: matchingDocuments)
     } catch {
+        NSLog("readAll failed: \(error.localizedDescription)")
         return makeErrorResponse(status: .internalServerError, error: error, response: path + ": readAll failed")
     }
 }
