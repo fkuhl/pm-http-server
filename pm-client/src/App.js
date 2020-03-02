2a9a8780f335fd2ebd194bccc7cb99a75a5c8a28
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2020-03-02 00:36:54 -0500

    Basic table with optional pagination

--- pm-client/src/App.js
+++ pm-client/src/App.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PMTable from './PMTable.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        members: [],
+        households: [],
+      },
+      currentView: 'members',
+      usePagination: true
+    }
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    fetch('/Members/readAll', {
+      credentials: 'include',
+      mode: 'cors'
+    })
+    .then(res => {
+      res.json()
+      .then(json => {
+        this.setState(prevState => {
+          let data = prevState.data;
+          data.members = json.map(x => x.value);;
+          return {data}
+          });
+      });
+    });
+
+    fetch('/Households/readAll', {
+      credentials: 'include',
+      mode: 'cors'
+    })
+    .then(res => {
+      res.json()
+      .then(json => {
+        this.setState(prevState => {
+          let data = prevState.data;
+          data.hoouseholds = json.map(x => x.value);
+          return {data}
+          });
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div >
+          <h2>Peri Meleon Demo</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="paginate">
+          <input type="checkbox"
+            onClick={(e) => this.setState({usePagination: e.target.checked})}
+            defaultChecked={this.state.usePagination}
+          />Paginate
+        </div>
+        <PMTable
+          data={this.state.data[this.state.currentView]}
+          usePagination={this.state.usePagination}
+        />
       </div>
     );
   }
--- pm-client/src/PMTable.js
+++ pm-client/src/PMTable.js
@@ -1,6 +1,187 @@
 import React from 'react'
-import ReactTable from 'react-table'
+import styled from 'styled-components'
+import { useTable, usePagination } from 'react-table'
 
-export default class PMTable extends React.Component {
 
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    margin auto;
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+
+  .pagination {
+    padding: 0.5rem;
+    margin: 0
+  }
+`
+
+function Table({ columns, data, showPagination }) {
+    
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    rows,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    usePagination
+  )
+
+  data = showPagination ? page : rows
+  return (
+    <div>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {data.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+    {showPagination ?
+    <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div> : null}
+      </div>
+  )
 }
+
+function PMTable(props) {
+  const columns = React.useMemo(
+    () => [
+        {
+            Header: 'Name',
+            accessor: rec => `${rec.givenName} ${rec.familyName}`
+        },
+        {
+            Header: 'Address',
+            accessor: rec => 'Somewhere'
+        },
+        {
+            Header: 'Phone Number',
+            accessor: 'mobilePhone'
+        },
+        {
+            Header: 'EMail',
+            accessor: 'eMail',
+        },
+        {
+            Header: 'Status',
+            accessor: 'status',
+        },
+        {
+            Header: 'Resident',
+            accessor: rec => rec.resident ? 'Yes': 'No'
+        },
+    ],
+    []
+  )
+
+//   const data = React.useMemo(() => props.data, [])
+
+  return (
+    <Styles>
+      <Table
+        columns={columns}
+        data={props.data}
+        showPagination={props.usePagination}
+      />
+    </Styles>
+  )
+}
+
+export default PMTable
